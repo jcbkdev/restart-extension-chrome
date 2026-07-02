68ec4095a358ac26e2ae3cf110ea769c56c9fcb3
jcbkdev <kubkon134@gmail.com>
2026-07-02 12:12:06 +0200

    feat(popup): display blocked urls

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,3 +1,4 @@
+import { BlockListRepository } from "./block_list/block_list_repository.js";
 import { PresetRepository } from "./preset/preset_repository.js";
 
 async function loadActivePresets() {
@@ -14,8 +15,27 @@ async function loadActivePresets() {
   });
 }
 
+async function loadCustomUrls() {
+  const repoBlockLists = await BlockListRepository.getAllLists();
+  const allBlockedUrls = repoBlockLists.flatMap((blockList) =>
+    blockList.getUrls(),
+  );
+
+  if (!allBlockedUrls || allBlockedUrls.length === 0) return;
+
+  const customUrlsListElement = document.getElementById("urlList");
+  if (!customUrlsListElement) return;
+
+  allBlockedUrls.forEach((url) => {
+    const listItemElement = document.createElement("li");
+    listItemElement.innerText = url;
+    customUrlsListElement.appendChild(listItemElement);
+  });
+}
+
 function initialize() {
   loadActivePresets();
+  loadCustomUrls();
 }
 
 initialize();
